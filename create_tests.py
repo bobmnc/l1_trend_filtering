4444c193b3c595ff55f3bb76386bc6d246a18e0c
bobmnc <bobomnc2b@gmail.com>
2023-12-26 16:20:02 +0100

    multivariate time series test function

--- create_tests.py
+++ create_tests.py
@@ -19,6 +19,27 @@ def create_signals(N : int,signal_length : int,max_slope : float,p_trend_change 
                 signals[k,j] = signals[k,j-1]+slope
     return signals
 
+def create_multivariate_signals(N : int,signal_length : int,
+                                D :int, 
+                                max_slope : float,
+                                p_trend_change : float,
+                                noise_level:float,shift :int =50):
+    '''create N signals of size signal_length that have linear trend 
+    and changes trend every 1/(1-p) in mean,
+    the dim of the signal is D 
+    and every signal is just a shift of the 1st signal
+    '''
+    signals =  np.zeros((N,D,signal_length))
+    
+    first_signal = create_signals(N,signal_length,max_slope,
+                                  p_trend_change,noise_level)
+    for k in range(D):
+        signals[:,k,:] = np.roll(first_signal,axis=1,
+                                 shift=shift*k)
+    
+    return signals
+
+
 if __name__=='__main__':
     signals = create_signals(N=1000,signal_length=2000,max_slope=4,p_trend_change=0.2,noise_level=1)
     np.save('./data/signals_test.npy',signals)
