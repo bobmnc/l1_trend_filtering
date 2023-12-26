2a583aeafd6a4913905d81a06c3e3d8cf2ada0a2
bobmnc <bobomnc2b@gmail.com>
2023-12-26 20:08:11 +0100

    added l1 trend filtering with A formulation

--- filter.py
+++ filter.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.linalg import toeplitz
 import cvxpy as cp
+import scipy
 
 
 def Hodrick_Prescott(signal,penalty):
@@ -28,6 +29,33 @@ def l1_trend_filter(signal,penalty):
     prob =  cp.Problem(objective=objective)
     prob.solve(solver = cp.CLARABEL)
     return x.value
+
+def l1_trend_filter2(signal,penalty):
+    n = len(signal)
+    theta = cp.Variable(n)
+    mat_A = compute_matrix_A(n)
+    print(mat_A.shape)
+    objective=  cp.Minimize(cp.sum_squares(mat_A@theta-signal)+penalty*cp.norm(theta[3:],1))
+
+    prob =  cp.Problem(objective=objective)
+    prob.solve(solver = cp.CLARABEL)
+    return mat_A@theta.value
+
+def compute_matrix_A(n):
+    A_ = np.diag(np.ones(n),0)
+    for k in range(2,n):
+        A_ += np.diag(k*np.ones(n-k+1),-k+1)
+    A_[:,0] = np.ones(n)
+    #A_ = scipy.sparse.dia_array(A_)
+
+    return A_
+
+
+A_ = compute_matrix_A(4)
+
+l1_trend_filter2(np.load('./data/signals_test.npy')[0],penalty=10) 
+
+
 
 
 
