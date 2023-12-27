1a32d3741f28e713611c54ed81985a3e79360016
bobmnc <bobomnc2b@gmail.com>
2023-12-27 17:54:20 +0100

    added formulation with A theta
    same formulation as in the paper with A and theta

--- multivariate_filters.py
+++ multivariate_filters.py
@@ -1,10 +1,12 @@
 import numpy as np
 from scipy.linalg import toeplitz
+from filter import compute_matrix_A
 from create_tests import create_multivariate_signals
 from tqdm import tqdm
 from numba import jit
 ## formulation 1
-@jit(nopython=False)
+## to do : numba compatible toeplitz
+#@jit(nopython=False)
 def m_focuss(signal : np.ndarray,penalty :float,n_iter :int = 20):
     n = signal.shape[0]
     dim = signal.shape[1]
@@ -28,6 +30,32 @@ def m_focuss(signal : np.ndarray,penalty :float,n_iter :int = 20):
         X = filtered
     
     return X
+
+def m_focuss_A(signal : np.ndarray,penalty :float,n_iter :int = 20):
+    n = signal.shape[0]
+    dim = signal.shape[1]
+    A  = compute_matrix_A(n)
+    theta  = np.clip(np.linalg.pinv(A)@signal,
+                     -0.1,0.1) # we do some hard thresholding (test)
+    P = np.eye(n-2,n,k=2) # projector in the space spanned by n-2 last vectors
+    AtA = A.T@A
+    Aty = A.T@signal
+
+    for k in tqdm(range(n_iter)):
+        W = np.diag(np.sqrt(np.linalg.norm(P@theta,
+                                   axis=1)))
+        W_inv_2 = np.clip(np.diag(1/np.linalg.norm(P@theta,
+                                   axis=1)),
+                                   -10**6,
+                                   10**6)
+        filtering_matrix = (AtA+penalty*P.T@W_inv_2@P)
+        filtering_matrix = np.clip(np.linalg.pinv(filtering_matrix),
+                                   -10**6,
+                                   10**6)
+        filtered = filtering_matrix@Aty
+        theta = filtered
+    
+    return A@theta
 
 if __name__=='__main__':
     signal_test = create_multivariate_signals(N =1,signal_length=1000,
