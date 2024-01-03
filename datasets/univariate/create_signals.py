697525568927a1bb99bb228b9594b1b7a608006c
Thomas LI <thomas.li9162@gmail.com>
2024-01-03 16:27:44 +0100

    update slope generation

--- datasets/univariate/create_signals.py
+++ datasets/univariate/create_signals.py
@@ -31,9 +31,9 @@ def create_signals(N : int,signal_length : int, max_slope : float,p_trend_change
             else:
                 # we make sure the new slope will be different enough from the old slope
                 potential_slope = (np.random.random()-.5)*2*max_slope
-                while np.abs(potential_slope - slope) / max_slope <= 0.33:
+                while np.abs(potential_slope - slope) / max_slope <= 0.25:
                     potential_slope = (np.random.random()-.5)*2*max_slope
-                slope = (np.random.random()-.5)*2*max_slope
+                slope = potential_slope
                 signals[k,j] = signals[k,j-1]+slope
 
     # adding noise
