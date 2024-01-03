38b0d7acaeae23ec0fd379d61b143485d0694dd0
Thomas LI <thomas.li9162@gmail.com>
2024-01-03 16:56:43 +0100

    repair multivariate signals

--- datasets/multivariate/create_multivariate_signals.py
+++ datasets/multivariate/create_multivariate_signals.py
@@ -1,22 +1,35 @@
 import numpy as np
+from datasets.univariate.create_signals import create_signals
 
 
-def create_multivariate_signals(N : int,signal_length : int,
-                                D :int, 
-                                max_slope : float,
-                                p_trend_change : float,
-                                noise_level:float,shift :int =50):
-    '''create N signals of size signal_length that have linear trend 
+def create_multivariate_signals(
+    N: int,
+    signal_length: int,
+    D: int,
+    max_slope: float,
+    p_trend_change: float,
+    noise_level: float,
+    shift: int = 50):
+    """create N signals (D dimensional) of size signal_length that have linear trend
     and changes trend every 1/(1-p) in mean,
-    the dim of the signal is D 
-    and every signal is just a shift of the 1st signal
-    '''
-    signals =  np.zeros((N,D,signal_length))
-    
-    first_signal = create_signals(N,signal_length,max_slope,
-                                  p_trend_change,noise_level)
+    the dim every signal is D
+    for every signal, we have one underlying "big" signal
+    and on every dimension is just the big signal + a smaller signal
+
+    output shape : (N, signal_length, D)
+    """
+    signals = np.zeros((N, signal_length, D))
+
+    big_signal, breakpoints_list = create_signals(
+        N, signal_length, max_slope, p_trend_change, noise_level
+    )
+
+    signals = np.tile(big_signal, (D, 1, 1))
+    signals = np.transpose(signals, (1, 2, 0))
+
     for k in range(D):
-        signals[:,k,:] = np.roll(first_signal,axis=1,
-                                 shift=shift*k)
-    signals = np.transpose(signals,(0,2,1))
-    return signals
+        small_signal, small_bkps = create_signals(
+            N, signal_length, max_slope, p_trend_change, noise_level
+        )
+        signals[:, :, k] += 0.3 * small_signal
+    return signals, breakpoints_list
