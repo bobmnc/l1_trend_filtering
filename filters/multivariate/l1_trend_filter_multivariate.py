702d506ca95d403f90fcf50361a8c53846712755
Thomas LI <thomas.li9162@gmail.com>
2024-01-03 17:24:38 +0100

    forgot the 2 in front of the penalty term

--- filters/multivariate/l1_trend_filter_multivariate.py
+++ filters/multivariate/l1_trend_filter_multivariate.py
@@ -13,7 +13,7 @@ def l1_trend_filter_multivariate(signal, penalty):
     right = 0
     for t in range(1, n - 1):
         right += cp.norm(x[t - 1] - 2 * x[t] + x[t + 1], 2)
-    objective = cp.Minimize(left + penalty * right)
+    objective = cp.Minimize(left + 2*penalty * right)
     prob = cp.Problem(objective=objective)
     prob.solve(solver=cp.CLARABEL)
     return x.value
