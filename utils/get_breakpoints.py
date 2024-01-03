0bcb6cc2ca5f2bac5beb50804710c6830c2950d6
Thomas LI <thomas.li9162@gmail.com>
2024-01-03 12:20:42 +0100

    adding get_breakpoint function

---
+++ utils/get_breakpoints.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+def get_breakpoints(trend):
+    """
+    Takes in argument the output of l1_trend or l1_trend_multivariate of one signal
+    and returns the breakpoints.
+    """
+    second_diff = np.convolve(trend, np.array([1,-2,1]), mode = 'valid')
+    breakpoints = np.where(1 - np.isclose(second_diff, 0., atol=1e-02))[0]
+    # get rid of consecutive breakpoints
+    if len(breakpoints) == 0:
+        breakpoints_list = []
+    else:
+        breakpoints_list = [breakpoints[0]]
+        for i in range(1,len(breakpoints)):
+            if breakpoints[i] != breakpoints[i-1] + 1:
+                breakpoints_list.append(breakpoints[i])
+                
+    return breakpoints_list
