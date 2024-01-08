fb335b8cef278f93c5874f982819c5e30b03865f
Thomas LI <thomas.li9162@gmail.com>
2024-01-08 21:36:10 +0100

    update get breakpoints to take normalized signals

--- utils/get_breakpoints.py
+++ utils/get_breakpoints.py
@@ -7,7 +7,7 @@ def get_breakpoints(trend):
     and returns the breakpoints.
     """
     second_diff = np.convolve(trend, np.array([1,-2,1]), mode = 'valid')
-    breakpoints = np.where(1 - np.isclose(second_diff, 0., atol=1e-02))[0]
+    breakpoints = np.where(1 - np.isclose(second_diff, 0., atol=5e-04))[0]
     # get rid of consecutive breakpoints
     if len(breakpoints) == 0:
         breakpoints_list = []
